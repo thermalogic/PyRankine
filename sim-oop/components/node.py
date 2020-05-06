069d269c5ebd7ac1a9bc86e260b4e6ae45948b24
Cheng Maohua <cmh@seu.edu.cn>
2020-05-06 12:00:25 +0800

    update node

--- sim-oop/components/node.py
+++ sim-oop/components/node.py
@@ -49,7 +49,7 @@ class Node:
         self.kwargs = Node.kwargs.copy()
         self.kwargs.update(dictnode)
         for key in ['p', 't', 'h', 's', 'v', 'x', 'fdot', 'mdot']:
-            if type(self.kwargs[key]) in [int,float]:
+            if type(self.kwargs[key]) is int:
                 self.kwargs[key] = float(self.kwargs[key])
         self.__dict__.update(self.kwargs)
 
