a66341603b6712d79580a9215fb81b7aaaf254a9
thermalogic <cmh@seu.edu.cn>
2020-05-31 11:52:33 +0800

    cleanup

--- sim-oop/rankine.py
+++ sim-oop/rankine.py
@@ -27,52 +27,50 @@ import json
 from rankinecycle.cycleobj import RankineCycle
 from rankinecycle.cyclehelper import OutFiles
 
+
 class SimRankineCycle:
 
     def __init__(self, rankinefilename):
-        self.prefixResultFileName = (
-            rankinefilename[0:-5]).replace("txtcycle", "output")  # -5 remove .json
-       
+       # -5 remove .json
+        self.prefixResultFileName =rankinefilename[0:-5].replace("txtcycle", "output")
         with open(rankinefilename, 'r') as f:
             self.idictcycle = json.loads(f.read())
         self.cycle = RankineCycle(self.idictcycle)
 
     def CycleSimulator(self):
         self.cycle.simulator()
-  
-    def CycleSpecifiedSimulator(self, SetPower=None,SetMass=None):
+
+    def CycleSpecifiedSimulator(self, SetPower=None, SetMass=None):
         # Specified Simulating： Power or Mass Flow
-        self.cycle.SpecifiedSimulator(SetPower,SetMass)
-        
+        self.cycle.SpecifiedSimulator(SetPower, SetMass)
+
         # output to files
-        if SetPower!=None:
-           outprefix= self.prefixResultFileName + '-sp'
+        if SetPower != None:
+            outprefix = self.prefixResultFileName + '-sp'
         else:
-           outprefix= self.prefixResultFileName + '-sm'
+            outprefix = self.prefixResultFileName + '-sm'
         # output to text
         OutFiles(self.cycle)
-        OutFiles(self.cycle,outprefix+ '.txt')
-        
-          
-if __name__=="__main__":
-   from platform import *
-   import glob
-   curpath = os.path.abspath(os.path.dirname(__file__))
-   json_filesname_str=curpath+'\\'+'./data/txtcycle/rankine8[0-9].json'
-   #json_filesname_str=curpath+'\\'+'./data/txtcycle/rankine85.json'
-   json_filesname=glob.glob(json_filesname_str)
+        OutFiles(self.cycle, outprefix + '.txt')
 
-   Wcycledot = 100 # MW
-   mdot= 150*3600 # kg/h  
-   for i in range(len(json_filesname)):
-      print(curpath)
-      cycle=SimRankineCycle(json_filesname[i])
-      # 1 1kg
-      cycle.CycleSimulator()
- 
-      # 2 Specified Net Output Power(MW)
-      cycle.CycleSpecifiedSimulator(SetPower=Wcycledot)      
- 
-      # 3 Specified Mass Flow(kg/h)
-      cycle.CycleSpecifiedSimulator(SetMass=mdot)   
-   
+
+if __name__ == "__main__":
+    from platform import *
+    import glob
+    curpath = os.path.abspath(os.path.dirname(__file__))
+    json_filesname_str = curpath+'\\'+'./data/txtcycle/rankine8[0-9].json'
+    # json_filesname_str=curpath+'\\'+'./data/txtcycle/rankine85.json'
+    json_filesname = glob.glob(json_filesname_str)
+
+    Wcycledot = 100  # MW
+    mdot = 150*3600  # kg/h
+    for curfile in json_filesname:
+        cycle = SimRankineCycle(curfile)
+        # 1 1kg
+        cycle.CycleSimulator()
+
+        # 2 Specified Net Output Power(MW)
+        cycle.CycleSpecifiedSimulator(SetPower=Wcycledot)
+
+        # 3 Specified Mass Flow(kg/h)
+        cycle.CycleSpecifiedSimulator(SetMass=mdot)
